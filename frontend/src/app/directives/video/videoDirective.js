692bcac2d4351269eaf47b43afb30b7a0dfb83e3
Johannes Wehner <johsw@information.dk>
2016-04-19 20:20:31 +0200

    Fix web-tv-url.

--- frontend/src/app/directives/video/videoDirective.js
+++ frontend/src/app/directives/video/videoDirective.js
@@ -5,10 +5,11 @@ app.directive('stiftenVideo', function(){
         restrict: 'AEC',
         templateUrl: 'app/directives/video/videoTemplate.html',
         controller: function ($scope, $sce) {
+
             $scope.trustSrc = function(src) {
               return $sce.trustAsResourceUrl(src);
             }
-            $scope.videoUrl = $scope.story.guid;
+            $scope.videoUrl = $scope.story.webtv_source;
         }
     };
 });
