c6a29f450f9c26960764958b2b14fb2ed1776152
Johannes Wehner <johsw@information.dk>
2016-04-19 20:01:58 +0200

    Make contact and staff same page.

--- frontend/src/app/pages/staffPage/staffPageController.js
+++
@@ -1,9 +0,0 @@
-/* global Hammer:true */
-
-'use strict';
-
-app.controller('ContactController', function ($scope, $rootScope) {
-
-  $rootScope.pageTypeClass = 'page-list-page page-contact-page';
-  $rootScope.$emit('tracking');
-});
